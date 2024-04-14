bc1f2482346a3b74b650e51c5868c354c2198983
Viktor Bezdenezhnykh <bv@leber.ru>
2024-04-14 21:07:44 +0500

    INT-5 sidebar, fixes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,18 +10,8 @@
 @endsection
 
 @section('content')
-    <div class="auth-wrapper auth-cover">
-        <div class="auth-inner d-flex flex-row m-0">
-            <div class="d-flex flex-column col-12 align-items-center auth-bg p-0">
-                <div class="content col-sm-9 col-md-6 col-lg-3" style="min-width: 300px;">
-                    <div class="login-island" style="padding-top: 50%">
-                        <div class="header d-lg-flex flex-column col-lg-12 align-items-center justify-content-center pb-4">
-                            <h1><span class="label">Dashboard</span></h1>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div class="d-flex flex-row justify-content-center pt-5">
+        <h1><span class="label">Dashboard</span></h1>
     </div>
 @endsection
 
